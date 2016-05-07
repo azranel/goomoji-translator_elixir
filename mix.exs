366663cbfbc59ed16eeba082f62b2e32da2d07f1
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-05-07 12:29:42 +0100

    Bump lib version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule GoomojiTranslator.Mixfile do
 
   def project do
     [app: :goomoji_translator,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
