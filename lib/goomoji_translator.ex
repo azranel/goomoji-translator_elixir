1822170f110749b151652b458be5dbcbb56d47e6
Bartosz Łęcki <bartosz.lecki@netguru.pl>
2016-07-08 16:05:26 +0200

    Use memoization
    Also refactor code to be more readable

--- lib/goomoji_translator.ex
+++ lib/goomoji_translator.ex
@@ -1,11 +1,22 @@
 defmodule GoomojiTranslator do
+  import DefMemo
+
   def find_by_goomoji_code(code) do
-    File.read!(Path.expand("./config/file.txt")) |>
-    String.split |>
-    Enum.map(fn x -> String.split(x, ";") end) |>
-    Enum.filter(fn x -> Enum.at(x, 0) == code end) |>
-    Enum.at(0) |>
-    tl |>
-    hd
+    parse_data_file
+      |> fetch_utf8_code_from_data(code)
+  end
+
+  defmemo parse_data_file do
+    File.read!(Path.expand("./config/file.txt"))
+      |> String.split
+      |> Enum.map(fn x -> String.split(x, ";") end)
+  end
+
+  defp fetch_utf8_code_from_data(data, code) do
+    data
+      |> Enum.filter(fn x -> Enum.at(x, 0) == code end)
+      |> Enum.at(0)
+      |> tl
+      |> hd
   end
 end
